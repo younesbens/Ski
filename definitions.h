d08c8dd1b5c3c1bd8970576a5d782cc4a9439af6
younesbens <younes.benyamna@gmail.com>
2016-04-08 13:14:19 +0200

    algo ajouté

--- definitions.h
+++ definitions.h
@@ -1,30 +1,36 @@
+#ifndef DEF_DEF
+#define DEF_DEF
+
+#endif
+
+
+
 //definition du nombre de sommets et du nombre d'arcs du graphe orienté représentant la carte
-#define V 5		//nombre de sommets
+#define V 6		//nombre de sommets
 #define E 7		//nombre d'arcs
-
-typedef struct Nom	
-{//Represente un nom
-	char* val;
-	struct Nom* suiv;
-} NOM;
-
-typedef NOM* NOMS;		//Represente les noms de tous les arcs d'un sommet vers un autres
-
-typedef struct Poid	
-{//Represente un poids
-	int val;
-	struct Poid* suiv;
-} POID;
-
-typedef POID* POIDS;	//Represente les poids de tous les arcs d'un sommet vers un autres
 
 typedef struct arc Arc;
 
 struct arc
 {
-	NOMS nom;		//noms des arcs, liste de noms à laquelle on ajoute un element à chaque nouveau arc
+	char*  nom;		//noms des arcs, liste de noms à laquelle on ajoute un element à chaque nouveau arc
 	char* depart;	//nom sommet depart
 	char* arrivee;	//nom sommet arrivee
-	POIDS poids;	//poids des arcs, liste de poids à laquelle on ajoute un element à chaque nouveau arc
+	int poids;	
 	int nbArcs;		//nombre d'arcs reliant le sommet de depart au sommet d'arrivee
 };
+
+struct parc{
+	int sommet;
+	int poid;
+	int parcouru;
+	};
+typedef struct parc parcour;
+
+struct ante{
+	int sommet;
+	int pere;
+	};
+typedef struct ante antecedant;
+
+	Arc G[V][V];
--- main.c
+++ main.c
@@ -1,16 +1,24 @@
 #include "graphe.h"
-#include <stdio.h>
+#include "fonctions.h"
+#include <stdio.h> 
 
 
 int main(int argc, char* argv[])
 {
-	Arc G[V][V];
-	
+	char depart[100];
+	char arrivee[100];
+	antecedant a[V];
+	parcour p[V]; 
 	lectureGraphe("graphe.txt",G);
-	
-	
-	//printf("%s\n",G[0][1].depart);
-	afficheGraphe(G);
-	
+	printf("A quel endroit vous situez vous ?\n");
+	scanf("%s",depart);
+	printf("A quel endroit voulez vous acceder ?\n");
+	scanf("%s",arrivee);
+	printf("\n\n\n");
+	initialise_tableau_antecedant(a);
+	initialise_tableau_parcour(p,indiceSommet(depart));
+	dijstra(indiceSommet(depart),indiceSommet(arrivee),a,p);   
 	return 0;
 }
+ //si poid arrivée==1000 demande de remonter un peu a pied car chemin inexistant
+ //le poid =1000 a modifier
